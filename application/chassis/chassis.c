e20c28d136cef7718dff091d322e258aeec8aafc
WScrx <157961000+WScrx@users.noreply.github.com>
2024-04-11 22:06:29 +0800

    更新超电，UI

--- application/chassis/chassis.c
+++ application/chassis/chassis.c
@@ -369,7 +369,6 @@ void ChassisTask()
                 offset_angle = chassis_cmd_recv.offset_angle - 180;
             chassis_cmd_recv.wz = PIDCalculate(&FollowMode_PID, offset_angle, 0);
 
-            // chassis_cmd_recv.wz = -1.1 * offset_angle * abs(offset_angle);
             break;
         case CHASSIS_ROTATE: // 自旋,同时保持全向机动;当前wz维持定值,后续增加不规则的变速策略
             chassis_cmd_recv.wz = 2200;
